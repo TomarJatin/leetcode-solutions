6b797d3293f5a8dd4dbbdcfee5f3fd43ca7ba278
TomarJatin <jt24072002@gmail.com>
2023-06-10 19:45:43 +0530

    485. Max Consecutive Ones

---
+++ 485.max-consecutive-ones.cc
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int ans =0;
+        for(int i=0; i<nums.size(); i++){
+            int count = 0;
+            while(i<nums.size() && nums[i] == 1){
+                count++;
+                i++;
+            }
+            ans = max(ans, count);
+        }
+        return ans;
+    }
+};
